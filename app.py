4a873445147cca71757cb0598e1888cd144c201e
ankurraj007 <rajguptaankur@gmail.com>
2026-03-13 20:10:34 +0530

    debug

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 # importing dependencies
 from dotenv import load_dotenv
 import streamlit as st
